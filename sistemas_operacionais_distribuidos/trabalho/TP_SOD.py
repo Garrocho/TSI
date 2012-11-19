7c31e1554b13609a3d70fab03d2eb62da48a3897
CharlesGarrocho <ctgarrocho@gmail.com>
2012-11-19 16:47:43 -0200

    Alterado: Geração do Salt do Arquivo.

--- sistemas_operacionais_distribuidos/trabalho/TP_SOD.py
+++ sistemas_operacionais_distribuidos/trabalho/TP_SOD.py
@@ -31,11 +31,12 @@ class ForcaBruta:
 		self.crpt = crpt
 
     def run(self):
-		for senha in self.combinacao:
-			if crypt.crypt(''.join(senha), self.salt) == self.crpt:
-				print 'Senha: {0}'.format(''.join(senha))
-				exit()
-		
+        for senha in self.combinacao:
+            print senha
+            if crypt.crypt(''.join(senha), self.salt) == self.crpt:
+                print 'Senha: {0}'.format(''.join(senha))
+                exit()
+
 def obterUsuarios():
 	"""
 	Obtem as linhas com os dados dos usuarios do arquivo /etc/shadow
@@ -54,22 +55,22 @@ def descobreSenha():
 	
 	senhas = []
 	salt = obterUsuarios()[0]
-	print salt
-	crpt = salt[3]
+	crpt = '${0}${1}${2}'.format(salt[1], salt[2], salt[3])
 	salt = '${0}${1}$'.format(salt[1], salt[2])
+	print salt
+	print crpt
 	
 	senhas.append(ForcaBruta(geraCombinacoes(NUMEROS, 6), crpt, salt))
-	senhas.append(ForcaBruta(geraCombinacoes(NUMEROS, 7), crpt, salt))
-	senhas.append(ForcaBruta(geraCombinacoes(LET_NUM, 6), crpt, salt))
-	senhas.append(ForcaBruta(geraCombinacoes(LET_NUM, 7), crpt, salt))
-	senhas.append(ForcaBruta(geraCombinacoes(LETRAS, 6), crpt, salt))
-	senhas.append(ForcaBruta(geraCombinacoes(LETRAS, 7), crpt, salt))
-	senhas.append(ForcaBruta(geraCombinacoes(ALFABETO, 6), crpt, salt))
-	senhas.append(ForcaBruta(geraCombinacoes(ALFABETO, 7), crpt, salt))
+	#senhas.append(ForcaBruta(geraCombinacoes(NUMEROS, 7), crpt, salt))
+	#senhas.append(ForcaBruta(geraCombinacoes(LET_NUM, 6), crpt, salt))
+	#senhas.append(ForcaBruta(geraCombinacoes(LET_NUM, 7), crpt, salt))
+	#senhas.append(ForcaBruta(geraCombinacoes(LETRAS, 6), crpt, salt))
+	#senhas.append(ForcaBruta(geraCombinacoes(LETRAS, 7), crpt, salt))
+	#senhas.append(ForcaBruta(geraCombinacoes(ALFABETO, 6), crpt, salt))
+	#senhas.append(ForcaBruta(geraCombinacoes(ALFABETO, 7), crpt, salt))
 	
 	for forca in senhas:
 	    Process(target=forca.run())
-		#Thread(target = forca.run()).start()
 
 if __name__ == '__main__':
-	Thread(target = descobreSenha).start()
+    Thread(target = descobreSenha).start()
