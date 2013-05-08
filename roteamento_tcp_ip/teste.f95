33872a2ba4bb819e59b82ef11e76b742eb6e22bf
CharlesGarrocho <charles.garrocho@gmail.com>
2013-05-08 11:15:21 -0300

    adicionado: nova versão, agora com leitura correta do arquivo.

--- roteamento_tcp_ip/teste.f95
+++ roteamento_tcp_ip/teste.f95
@@ -1,8 +1,13 @@
 PROGRAM TESTE
-    INTEGER :: verifica, arq = 10
+    INTEGER :: verifica, fim, arq = 10
+    CHARACTER*10 :: linha
     OPEN(arq, FILE='./dados.txt', STATUS='OLD', ACCESS='SEQUENTIAL', IOSTAT=verifica)
-    IF (verifica /= 0) THEN
-        PRINT *, 'OK'
+    IF (verifica == 0) THEN
+        DO
+            READ(arq, '(A)', IOSTAT=fim) linha
+            IF (fim < 0) EXIT
+            PRINT *, linha
+        END DO
     ELSE
         PRINT *, 'ERRO AO ABRIR O ARQUIVO'
     END IF
