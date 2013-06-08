0dc8bc2173e8466823bc7b222fcfacc3659508fa
CharlesGarrocho <charles.garrocho@gmail.com>
2013-06-08 11:02:55 -0300

    adicionado: função para validar cpf. #3

--- logica_programacao/trabalho/source/trabalho.pas
+++ logica_programacao/trabalho/source/trabalho.pas
@@ -49,3 +49,48 @@ var arqdep:tarqdep;
 	arqhistdep:tarqhistdep;
 	arqhistfunc:tarqhistfunc;
 	op,opc,opd,ope:char;
+	
+
+//BORDA
+procedure borda;
+var i:integer;
+begin
+     for i:=2 to 63 do
+     begin
+          gotoxy(i,2);
+          write('-');
+     end;
+     for i:=2 to 63 do
+     begin
+          gotoxy(i,21);
+          write('-');
+     end;
+end;
+
+//FUNCAO VALIDA CPF
+function validacpf(cpf:string):boolean;
+var cp,aux:string ;
+    vet:array[1..11] of integer;
+    Soma1,Soma2,d1,d2,i,convaux:integer; 
+Begin
+	    cp:=cpf;
+        for i:=1 to 11 do
+        begin
+                aux:=cp[i];
+                val(aux,convaux);
+                vet[i]:=convaux;
+        end;  
+        Soma1:= (Vet[1]*10)+(Vet[2]*9)+(vet[3]*8)+(vet[4]*7)+(vet[5]*6)+(vet[6]*5)+(vet[7]*4)+(vet[8]*3)+(vet[9]*2);
+        d1:= 11 -( soma1 mod 11);
+        If (d1 = 10) or (d1=11) then
+                D1:=0;
+        Soma2:= (Vet[1]*11)+(Vet[2]*10)+(vet[3]*9)+(vet[4]*8)+(vet[5]*7)+(vet[6]*6)+(vet[7]*5)+(vet[8]*4)+(vet[9]*3)+(D1*2);    
+        d2:= 11- (Soma2 mod 11);
+        if (d2=10) or (d2=11) then
+        d2:=0;
+        
+        If (Vet[10] =D1) and (Vet[11] = D2) Then
+           validacpf:=true
+        Else
+        validacpf:=false;
+end;
