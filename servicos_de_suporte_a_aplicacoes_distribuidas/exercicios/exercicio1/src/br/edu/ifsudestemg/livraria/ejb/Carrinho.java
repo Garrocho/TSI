8e12c8237f7484c31f356863768aaba2659d6eea
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-03 09:14:46 -0300

    adicionado: tag remote. #7

--- servicos_de_suporte_a_aplicacoes_distribuidas/exercicios/exercicio1/src/br/edu/ifsudestemg/livraria/ejb/Carrinho.java
+++ servicos_de_suporte_a_aplicacoes_distribuidas/exercicios/exercicio1/src/br/edu/ifsudestemg/livraria/ejb/Carrinho.java
@@ -2,8 +2,11 @@ package br.edu.ifsudestemg.livraria.ejb;
 
 import java.util.List;
 
+import javax.ejb.Remote;
+
 import br.edu.ifsudestemg.livraria.modelo.Livro;
 
+@Remote
 public interface Carrinho {
 	
 	void addLivro(Livro livro);
