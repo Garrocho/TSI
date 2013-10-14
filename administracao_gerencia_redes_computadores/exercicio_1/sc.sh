f151cea540bfa5b6b28d800ed285b3cb32c249d2
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-14 09:42:24 -0300

    adicionado: comando para criação do usuário. #10

--- administracao_gerencia_redes_computadores/exercicio_1/sc.sh
+++ administracao_gerencia_redes_computadores/exercicio_1/sc.sh
@@ -10,5 +10,5 @@ do
    dir_pai=`echo $nome_usuario | cut -b 1`
    senha=`perl -e 'print crypt('$senha',q($5$salt$)), "\n";'`
    sudo mkdir -p /home/$dir_pai
-
+   sudo useradd -m -s /bin/bash -k /etc/skel -c $descricao $nome_usuario -p $senha -d /home/$dir_pai/$nome_usuario
 done < user_pass.txt
