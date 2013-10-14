e095e346bee45ac541376a496c1e1eea13739141
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-14 09:40:58 -0300

    adicionado: separacao de : das variáveis. #10

--- administracao_gerencia_redes_computadores/exercicio_1/sc.sh
+++ administracao_gerencia_redes_computadores/exercicio_1/sc.sh
@@ -2,5 +2,10 @@
 
 while read linha 
 do  
+   nome_usuario=`echo $linha | cut -d ':' -f 1`
+   nome_completo=`echo $linha | cut -d ':' -f 2`
+   descricao=`echo $linha | cut -d ':' -f 3`
+   matricula=`echo $linha | cut -d ':' -f 4`
+   senha=`echo $linha | cut -d ':' -f 5`
 
 done < user_pass.txt
