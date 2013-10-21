ba6491516ebcddc68c654accee8b8e3e2d3c6207
CharlesGarrocho <charles.garrocho@gmail.com>
2013-10-21 10:22:50 -0200

    adicionado: função para cadastrar um compromisso. #11

--- programacao_para_dispositivos_moveis_e_sem_fio/exercicios/exercicio_1/static/js/script.js
+++ programacao_para_dispositivos_moveis_e_sem_fio/exercicios/exercicio_1/static/js/script.js
@@ -1,21 +1,39 @@
 $(document).ready(function() {
-    var ids = localStorage['ids'];
+    var ids = localStorage.getItem('ids');
     if (ids == null) {
-        ids = [1];
+        ids = 0;
+        localStorage.setItem('ids', ids);
         var comp = {};
-        for (i in ids) {
-            comp[ids[i]] = {
-                'titulo': 'Reunião Importante',
-                'texto': 'reuniao com os conselheiros...' };
-            localStorage['ids'] = JSON.stringify(ids);
-            localStorage['comp'] = JSON.stringify(comp);
-        }
+        comp[ids] = {
+            'titulo': 'Reunião Importante',
+            'texto': 'reuniao com os conselheiros...' };
+        localStorage['comp'] = JSON.stringify(comp);
+        $('#lista').append('<div data-role="collapsible"><h4>' + comps[0]['titulo'] + '</h4> <p>' + comps[0]['texto'] + '</p> </div>');
     }
     else {
-        ids = JSON.parse(ids);
         var comps = JSON.parse(localStorage.getItem('comp'));
-        for (i in ids) {
-            $('#lista').append('<div data-role="collapsible"><h4>' + comps[ids[i]]['titulo'] + '</h4> <p>' + comps[ids[i]]['texto'] + '</p> </div>');
+        var i;
+        for (i=0; i <= ids; i++) {
+            $('#lista').append('<div data-role="collapsible"><h4>' + comps[i]['titulo'] + '</h4> <p>' + comps[i]['texto'] + '</p> </div>');
         }
     }
 });
+
+function cadastrar() {
+    var titulo = document.getElementById('titulo_compromisso').value;
+    var texto = document.getElementById('texto_compromisso').value;
+
+    if (titulo.length > 0 && texto.length > 0) {
+        var ids = parseInt(localStorage.getItem('ids'))+1;
+        var comps = JSON.parse(localStorage['comp']);
+        comps[ids] = {
+            'titulo': titulo,
+            'texto': texto };
+        localStorage.setItem('ids', ids);
+        localStorage['comp'] = JSON.stringify(comps);
+        $('#lista').append('<div data-role="collapsible"><h4>' + comps[ids]['titulo'] + '</h4> <p>' + comps[ids]['texto'] + '</p> </div>');
+    }
+    else {
+        alert('Preencha Todos Os Campos do Cadastro de Compromissos!');
+    }
+}
