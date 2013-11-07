9b01a0bb8ec2ae54afc5be33c44a3e8d8fbc01ff
CharlesGarrocho <charles.garrocho@gmail.com>
2013-11-07 15:22:04 -0200

    adicionado: exercício para scannear portas tcp.

---
+++ seguranca_sistemas_computacionais/exercicios/exercicio4/scanner_porta.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+
+import socket
+import threading
+from sys import argv
+from re import findall
+
+
+TIME_OUT = 1 # Tempo limite de resposta do host.
+MAX_THREADS = 50 # Maximo de threads que poderam ser criadas.
+
+		
+def pesquisaPorta(porta):
+	arquivo = open('/etc/services').readlines()
+	for linha in arquivo:
+		if findall('\W\s{0}/tcp'.format(porta), linha):
+			print linha[:linha.index('\n')]
+
+
+class Scan(threading.Thread):
+    
+	def __init__(self, host):
+		threading.Thread.__init__(self)
+		self.host = host
+		self.portas = []
+		
+	def adicionaPorta(self, porta):
+		self.portas.append(porta)
+
+	def run(self):
+		while self.portas:
+			porta = self.portas.pop()
+			soquete = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			soquete.settimeout(TIME_OUT)
+			resposta = soquete.connect_ex((self.host, porta))
+			soquete.close()
+			if resposta == 0:
+				pesquisaPorta(porta)
+
+
+def portaScanner(host, portaInicio, portaFim):
+	print 'HOST: {0}\nPORTAS TCP: {1} a {2}\n\nPORTAS ABERTAS:'.format(host, portaInicio, portaFim)
+	portas = range(int(portaInicio), int(portaFim) + 1)
+	porcentagem = (float(len(portas)) / MAX_THREADS)
+	scanners = []
+	while portas:		
+		auxPor = 0
+		scanner = Scan(host)
+		while auxPor < porcentagem and portas:
+			auxPor += 1
+			scanner.adicionaPorta(int(portas.pop()))
+		scanners.append(scanner)
+		
+	scanners.reverse()
+	for i in range(len(scanners)):
+		threading.Thread(target = scanners[i].run()).start()
+
+
+if __name__ == '__main__':
+	
+	if len(argv) == 4:
+		try:
+			portaInicio = int(argv[2])
+			portaFim = int(argv[3])
+			if portaFim > 65535 or portaInicio > portaFim :
+				print 'Portas Invalidas.\nUse: python Exercicio_5_1.py <dominio ou ip> <porta_inicio> <porta_fim>'
+			else:
+				portaScanner( argv[1], portaInicio, portaFim )
+		except:
+			print 'Você deve fornecer numeros inteiros para as portas.\nUse: python Exercicio_5_1.py <dominio ou ip> <porta_inicio> <porta_fim>'
+	elif len(argv) == 2:
+		portaScanner( argv[1], 1, 65535)
+	else:
+		print 'Erro de entrada.\nUse: python Exercicio_5_1.py <dominio ou ip> <porta_inicio> <porta_fim>'
